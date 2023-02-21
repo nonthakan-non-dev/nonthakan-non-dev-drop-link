ec8b20880c94f8064374f8fb36a5a5d44d42e7c6
nonthakan@kkumail.com <nonthakan@kkumail.com>
2023-02-21 22:51:37 +0700

    [update] set link in logo

--- src/components/LayoutAuth.js
+++ src/components/LayoutAuth.js
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import Menus from "./Menu";
 // boxicons
 
 const LayoutAuth = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <div className="w-screen">
       <div className="bg-white min-h-screen	w-full bg-gradient-to-r from-silver to-white px-[1rem] box-border">
         <div className="sticky top-[15px] pr-[15px] w-full flex flex-row justify-between">
-          <div className="flex items-center justify-center ml-3">
+          <div
+            className="flex items-center justify-center ml-3 cursor-pointer"
+            onClick={() => navigate("/", { replace: true })}
+          >
             <div className="h-[30px]">
               <img
                 src={process.env.PUBLIC_URL + "/images/droplink-logo.png"}
