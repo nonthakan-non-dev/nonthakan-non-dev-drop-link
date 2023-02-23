d81c3911699fac8c928d4393de8723a2a5d91282
nonthakan@kkumail.com <nonthakan@kkumail.com>
2023-02-23 22:46:37 +0700

    [update] setup popup

---
+++ src/components/DropLink.js
@@ -0,0 +1,132 @@
+import React from "react";
+import Modal from "@mui/material/Modal";
+import { useForm } from "react-hook-form";
+const DropLink = ({ modalIsOpen, setIsOpen }) => {
+  const handleClose = () => setIsOpen(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const style = {
+    position: "absolute",
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  };
+  return (
+    <div>
+      <Modal
+        open={modalIsOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div style={style}>
+          <form
+            className="bg-white border-0 rounded-lg px-8 pt-6 pb-8 mb-4 w-full h-full"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex items-center justify-center mb-8">
+              <div className="h-[65px] ">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/droplink-logo.png"}
+                  alt=""
+                  className="object-cover h-full w-auto"
+                />
+              </div>
+              <h1 className="text-2xl lg:text-4xl ml-4 select-none">
+                Drop Link
+              </h1>
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="text"
+                placeholder="name@gmail.com"
+                {...register("email", {
+                  required: "Please type your email.",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address.",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs italic mt-3">
+                  {errors?.email?.message}
+                </p>
+              )}
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                {...register("password", {
+                  required: "Please type your password.",
+                  minLength: {
+                    message: "Password has a minimum length of 6.",
+                    value: 6,
+                  },
+                  maxLength: {
+                    message: "Password has a maximum length of 20.",
+                    value: 20,
+                  },
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs italic mt-3">
+                  {errors?.password?.message}
+                </p>
+              )}
+            </div>
+            <div className="w-full mt-5">
+              <button
+                className="w-full bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+
+            <div className="flex items-center justify-center mt-8 text-sm ">
+              <p className="mr-2">Already have an account?</p>
+              <a className="text-blue-500 hover:text-blue-800" href="/">
+                Login
+              </a>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default DropLink;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,89 +1,90 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import _ from "lodash";
 import ProjectCard from "../components/ProjectCard";
 import ShortcutMenu from "../components/ShortcutMenu";
-import Swal from "sweetalert2";
-import { saveLink } from "../firebase";
+// import Swal from "sweetalert2";
+// import { saveLink } from "../firebase";
+import DropLink from "../components/DropLink";
 
 function Home() {
-  const [createPopup, setCreatePopup] = useState(false);
+  const [createPopup, setCreatePopup] = useState(true);
 
-  useEffect(() => {
-    saveLink({
-      image: "http://sweetalert2.github.io/images/sweetalert2-social.png",
-      tags: "html,css,js",
-      text: "A beautiful, responsive, customizable and accessib…AI-ARIA) replacement for JavaScript's popup boxes",
-      title: "SweetAlert2",
-    });
-    if (createPopup) {
-      Swal.fire({
-        title: "Drop your link",
-        input: "text",
-        inputAutoTrim: true,
-        inputPlaceholder: "https://www.link.com",
-        inputAttributes: {
-          autocapitalize: "off",
-        },
-        inputValidator: (value) => {
-          if (!value) {
-            return "You need to drop link!";
-          }
-          const httpRegex =
-            // eslint-disable-next-line
-            /^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
-          if (!httpRegex.test(value)) {
-            return "Invalid type URL!";
-          }
-        },
-        showCancelButton: true,
-        confirmButtonText: "Next",
-        confirmButtonColor: "#3B82F6",
-        showLoaderOnConfirm: true,
-        preConfirm: (url) => {
-          return fetch(`${process.env.REACT_APP_LINKPREVIEW_ENDPOINT}${url}`)
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error(response.statusText);
-              }
-              return response.json();
-            })
-            .catch((error) => {
-              Swal.showValidationMessage(`Request failed: ${error}`);
-            });
-        },
-        allowOutsideClick: () => !Swal.isLoading(),
-      }).then((resultLink) => {
-        if (resultLink.isConfirmed) {
-          Swal.fire({
-            title: `${resultLink?.value?.title ?? "-"}`,
-            text: `${resultLink?.value?.description ?? "-"}`,
-            imageUrl: `${
-              resultLink?.value?.image ?? process.env.REACT_APP_NO_IMAGE
-            }`,
-            input: "text",
-            inputPlaceholder: "Comma seperated tags e.g. work,dev",
-            inputAutoTrim: true,
-            inputAttributes: {
-              autocapitalize: "off",
-            },
-            showCancelButton: true,
-            confirmButtonText: "Save",
-            confirmButtonColor: "#3B82F6",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              const data = {
-                title: resultLink?.value?.title,
-                text: resultLink?.value?.description,
-                image: resultLink?.value?.image,
-                tags: result?.value,
-              };
-              saveLink(data);
-            }
-          });
-        }
-      });
-    }
-  }, [createPopup]);
+  // useEffect(() => {
+  //   saveLink({
+  //     image: "http://sweetalert2.github.io/images/sweetalert2-social.png",
+  //     tags: "html,css,js",
+  //     text: "A beautiful, responsive, customizable and accessib…AI-ARIA) replacement for JavaScript's popup boxes",
+  //     title: "SweetAlert2",
+  //   });
+  //   if (createPopup) {
+  //     Swal.fire({
+  //       title: "Drop your link",
+  //       input: "text",
+  //       inputAutoTrim: true,
+  //       inputPlaceholder: "https://www.link.com",
+  //       inputAttributes: {
+  //         autocapitalize: "off",
+  //       },
+  //       inputValidator: (value) => {
+  //         if (!value) {
+  //           return "You need to drop link!";
+  //         }
+  //         const httpRegex =
+  //           // eslint-disable-next-line
+  //           /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
+  //         if (!httpRegex.test(value)) {
+  //           return "Invalid type URL!";
+  //         }
+  //       },
+  //       showCancelButton: true,
+  //       confirmButtonText: "Next",
+  //       confirmButtonColor: "#3B82F6",
+  //       showLoaderOnConfirm: true,
+  //       preConfirm: (url) => {
+  //         return fetch(`${process.env.REACT_APP_LINKPREVIEW_ENDPOINT}${url}`)
+  //           .then((response) => {
+  //             if (!response.ok) {
+  //               throw new Error(response.statusText);
+  //             }
+  //             return response.json();
+  //           })
+  //           .catch((error) => {
+  //             Swal.showValidationMessage(`Request failed: ${error}`);
+  //           });
+  //       },
+  //       allowOutsideClick: () => !Swal.isLoading(),
+  //     }).then((resultLink) => {
+  //       if (resultLink.isConfirmed) {
+  //         Swal.fire({
+  //           title: `${resultLink?.value?.title ?? "-"}`,
+  //           text: `${resultLink?.value?.description ?? "-"}`,
+  //           imageUrl: `${
+  //             resultLink?.value?.image ?? process.env.REACT_APP_NO_IMAGE
+  //           }`,
+  //           input: "text",
+  //           inputPlaceholder: "Comma seperated tags e.g. work,dev",
+  //           inputAutoTrim: true,
+  //           inputAttributes: {
+  //             autocapitalize: "off",
+  //           },
+  //           showCancelButton: true,
+  //           confirmButtonText: "Save",
+  //           confirmButtonColor: "#3B82F6",
+  //         }).then((result) => {
+  //           if (result.isConfirmed) {
+  //             const data = {
+  //               title: resultLink?.value?.title,
+  //               text: resultLink?.value?.description,
+  //               image: resultLink?.value?.image,
+  //               tags: result?.value,
+  //             };
+  //             saveLink(data);
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  // }, [createPopup]);
 
   const AllLinks = () => {
     return (
@@ -106,6 +107,7 @@ function Home() {
   return (
     <div className="min-h-screen	w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4">
       <AllLinks />
+      <DropLink modalIsOpen={createPopup} setIsOpen={setCreatePopup} />
       <ShortcutMenu event={setCreatePopup} />
     </div>
   );
